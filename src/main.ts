74a18d62a2e5110bd6016de8cd546fdce1edfd70
maythitirat <thitiratmay.t@gmail.com>
2024-03-25 23:34:02 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ImATeapotException } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+  const whitelist = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:8000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:3001',
+    'http://127.0.0.1:3002',
+    'http://10.0.2.2:3000',
+  ];
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      origin: function (origin, callback) {
+        if (!origin) {
+          callback(null, true);
+          return;
+        }
+        if (
+          whitelist.includes(origin) || // Checks your whitelist
+          !!origin.match(/yourdomain\.com$/) // Overall check for your domain
+        ) {
+          console.log('allowed cors for:', origin);
+          callback(null, true);
+        } else {
+          console.log('blocked cors for:', origin);
+          callback(new ImATeapotException('Not allowed by CORS'), false);
+        }
+      },
+    },
   });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
   // app.enableCors({
   //   origin: true,
   //   // methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
