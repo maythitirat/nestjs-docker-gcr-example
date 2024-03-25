caa4b9f6854d476b1c71fdbe24e0f1ce075c2f39
maythitirat <thitiratmay.t@gmail.com>
2024-03-25 23:23:03 +0700

    add try catch

--- src/testapp/testapp.service.ts
+++ src/testapp/testapp.service.ts
@@ -34,57 +34,61 @@ export class TestappService {
     body: IRequestPackListBody;
   }): Promise<any> {
     console.log('body: ', body);
-    if (!useMock) {
-      const headersRequest = {
-        Sessionid: 'O2024022222381701113239',
-        Sourcesystemid: 'ESVWEB',
-      };
-      const { data } = await firstValueFrom(
-        this.httpService
-          .post(
-            'https://ddchpackagetest.test.dtac.co.th/api/upsell/packlistv2/requestPackListV3',
-            body,
-            { headers: headersRequest },
-          )
-          .pipe(
-            catchError((error: AxiosError) => {
-              console.log(error.response.data);
-              throw new HttpException(
-                error.response.data,
-                // {
-                //   status: {
-                //     statusType: 'B',
-                //     errorCode: 'B-UPS-71023',
-                //     errorMessage: "Subscriber's credit is inadequate",
-                //     server: 'kalium15.tac.co.th',
-                //     hostId: '38263b08dc21',
-                //     transactionId: 'ESVWEB20240223010159319917',
-                //   },
-                //   data: {
-                //     subrnumb: '66949296202',
-                //     resultmsg:
-                //       "Your transaction can't be completed. This number has insufficient balance.",
-                //     packcode: '21200496',
-                //     offerFlag: '0',
-                //     wootricInfo: {
-                //       dtacId:
-                //         'c14b848000e906a7579116487851d37713d193604befc5ae0b25cc443fc3652d',
-                //       journey: 'insufficient',
-                //     },
-                //   },
-                // },
-                HttpStatus.BAD_REQUEST,
-              );
-            }),
-          ),
-      );
-      return data;
-    } else {
-      return body.subrNumb && body.brand == Brand.Dtac
-        ? packlistDtac
-        : body.subrNumb && body.brand == Brand.True
-          ? packlistTrue
-          : packlistGuestMode;
+    try {
+      if (!useMock) {
+        const headersRequest = {
+          Sessionid: 'O2024022222381701113239',
+          Sourcesystemid: 'ESVWEB',
+        };
+        const { data } = await firstValueFrom(
+          this.httpService
+            .post(
+              'https://ddchpackagetest.test.dtac.co.th/api/upsell/packlistv2/requestPackListV3',
+              body,
+              { headers: headersRequest },
+            )
+            .pipe(
+              catchError((error: AxiosError) => {
+                console.log(error.response.data);
+                throw new HttpException(
+                  error.response.data,
+                  // {
+                  //   status: {
+                  //     statusType: 'B',
+                  //     errorCode: 'B-UPS-71023',
+                  //     errorMessage: "Subscriber's credit is inadequate",
+                  //     server: 'kalium15.tac.co.th',
+                  //     hostId: '38263b08dc21',
+                  //     transactionId: 'ESVWEB20240223010159319917',
+                  //   },
+                  //   data: {
+                  //     subrnumb: '66949296202',
+                  //     resultmsg:
+                  //       "Your transaction can't be completed. This number has insufficient balance.",
+                  //     packcode: '21200496',
+                  //     offerFlag: '0',
+                  //     wootricInfo: {
+                  //       dtacId:
+                  //         'c14b848000e906a7579116487851d37713d193604befc5ae0b25cc443fc3652d',
+                  //       journey: 'insufficient',
+                  //     },
+                  //   },
+                  // },
+                  HttpStatus.BAD_REQUEST,
+                );
+              }),
+            ),
+        );
+        return data;
+      } else {
+        return body.subrNumb && body.brand == Brand.Dtac
+          ? packlistDtac
+          : body.subrNumb && body.brand == Brand.True
+            ? packlistTrue
+            : packlistGuestMode;
+      }
+    } catch (error) {
+      console.log('error : ', error);
     }
   }
   // async requestPackList(body: IRequestPackListBody): Promise<any> {
