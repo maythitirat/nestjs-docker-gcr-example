67c0854393229b61670aae3f0dc2786b445aa14c
maythitirat <thitiratmay.t@gmail.com>
2024-03-25 20:23:30 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   // app.enableCors({
   //   origin: 'https://nestjs-docker-gcr-example-a5lmnu7zia-de.a.run.app',
   // });
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://nestjs-docker-gcr-example-a5lmnu7zia-de.a.run.app',
-      'http://localhost:3002',
-    ],
-    allowedHeaders: ['Accept', 'Content-Type'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://nestjs-docker-gcr-example-a5lmnu7zia-de.a.run.app',
+  //     'http://localhost:3002',
+  //   ],
+  //   allowedHeaders: ['Accept', 'Content-Type'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   credentials: true,
+  // });
   await app.listen(parseInt(process.env.PORT) || 3002);
 }
 bootstrap();
