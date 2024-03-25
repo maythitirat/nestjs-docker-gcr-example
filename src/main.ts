49c5db4a9558322f510bf6f1768904ef2f8393d6
maythitirat <thitiratmay.t@gmail.com>
2024-03-25 21:54:48 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     // methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    // credentials: true,
+    credentials: true,
     // allowedHeaders:
     //   'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
