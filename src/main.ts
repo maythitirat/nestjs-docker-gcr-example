2a415a9ae51760347bcc7a7dbe4f1ac749120e4c
maythitirat <thitiratmay.t@gmail.com>
2024-03-25 20:06:16 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   // app.enableCors({
   //   origin: 'https://nestjs-docker-gcr-example-a5lmnu7zia-de.a.run.app',
   // });
