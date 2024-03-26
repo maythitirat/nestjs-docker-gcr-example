4f0e2cc45a501bb217b2af288481cf2384148339
maythitirat <thitiratmay.t@gmail.com>
2024-03-26 09:31:14 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ImATeapotException } from '@nestjs/common';
 
 async function bootstrap() {
-  const whitelist = [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'http://localhost:8000',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:3001',
-    'http://127.0.0.1:3002',
-    'http://10.0.2.2:3000',
-  ];
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://www.maythitirat.com'],
+  });
+
+  // app.enableCors();
   // app.enableCors({
-  //   origin: '*',
+  //   origin: true,
   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   //   preflightContinue: false,
   //   optionsSuccessStatus: 204,
